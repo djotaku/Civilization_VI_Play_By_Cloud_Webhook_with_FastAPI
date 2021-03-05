857ab091f37c6d91227eec94d33d9c36c72ada45
Eric Mesa <ericsbinaryworld@gmail.com>
2021-03-05 18:53:10 -0500

    Fixing shell script for matrix bot

--- start_matrix_bot.sh
+++ start_matrix_bot.sh
@@ -4,4 +4,4 @@ echo "Start the virtual environment."
 source venv/bin/activate
 
 echo "Start the listener"
-python civ_vi_webhook.services.matrix.matrix_bot_listener.py
+python -m civ_vi_webhook.services.matrix.matrix_bot_listener.py
