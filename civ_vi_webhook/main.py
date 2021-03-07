34c0cbd07d16b5bbdba1c447d97b7ce8d82295e7
Eric Mesa <ericsbinaryworld@gmail.com>
2021-03-07 13:16:56 -0500

    Much happier to reduce duplicated code in /webhook
    Close #14

--- civ_vi_webhook/main.py
+++ civ_vi_webhook/main.py
@@ -94,6 +94,7 @@ def handle_play_by_cloud_json(play_by_cloud_game: CivTurnInfo):
     The reason for the duplication checks here are in case more than one player has the webhook enabled for all turns.
     That may be desirable because, for example, right now Mac users have crashes when using the webhook.
     """
+    send_message = False
     api_logger.debug(f'JSON from Play By Cloud: {play_by_cloud_game}')
     turn_time = datetime.now()
     game_name = play_by_cloud_game.value1
@@ -102,34 +103,17 @@ def handle_play_by_cloud_json(play_by_cloud_game: CivTurnInfo):
     if game_name in current_games.keys():
         if current_games[game_name]['player_name'] != player_name:
             api_logger.debug("Game exists, but this is not a duplicate")
-            message = f"Hey, {player_name}, it's your turn in {game_name}. The game is on turn {turn_number}"
-            current_games[game_name] = {'player_name': player_name,
-                                        'turn_number': turn_number,
-                                        'time_stamp': {'year': turn_time.year,
-                                                       'month': turn_time.month,
-                                                       'day': turn_time.day,
-                                                       'hour': turn_time.hour,
-                                                       'minute': turn_time.minute,
-                                                       'second': turn_time.second}}
-            api_matrix_bot.main(message)
+            send_message = True
         elif current_games[game_name]['turn_number'] != turn_number:
             api_logger.debug("A turn in a two-player game was somehow missed.")
-            message = f"Hey, {player_name}, it's your turn in {game_name}. The game is on turn {turn_number}"
-            current_games[game_name] = {'player_name': player_name,
-                                        'turn_number': turn_number,
-                                        'time_stamp': {'year': turn_time.year,
-                                                       'month': turn_time.month,
-                                                       'day': turn_time.day,
-                                                       'hour': turn_time.hour,
-                                                       'minute': turn_time.minute,
-                                                       'second': turn_time.second}}
-            api_matrix_bot.main(message)
+            send_message = True
         else:
             api_logger.debug("Game exists and this is a duplicate entry.")
             raise HTTPException(status_code=429, detail="Game exists and this is a duplicate entry.")
     else:
         api_logger.debug("New game.")
-        message = f"Hey, {player_name}, it's your turn in {game_name}. The game is on turn {turn_number}"
+        send_message = True
+    if send_message:
         current_games[game_name] = {'player_name': player_name,
                                     'turn_number': turn_number,
                                     'time_stamp': {'year': turn_time.year,
@@ -138,9 +122,10 @@ def handle_play_by_cloud_json(play_by_cloud_game: CivTurnInfo):
                                                    'hour': turn_time.hour,
                                                    'minute': turn_time.minute,
                                                    'second': turn_time.second}}
+        message = f"Hey, {player_name}, it's your turn in {game_name}. The game is on turn {turn_number}"
         api_matrix_bot.main(message)
-    with open('most_recent_games.json', 'w') as most_recent_games_file:
-        json.dump(current_games, most_recent_games_file)
+        with open('most_recent_games.json', 'w') as most_recent_games_file:
+            json.dump(current_games, most_recent_games_file)
 
 
 @app.post('/pydt', status_code=status.HTTP_201_CREATED)
