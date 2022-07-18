0cf714b8ea8a5baf753122bd7231890f6385de38
Eric Mesa <ericsbinaryworld@gmail.com>
2022-07-17 21:45:32 -0400

    Deleting shell scripts and requirements.txt are no longer relevant

--- start_matrix_bot.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Start the virtual environment."
-source venv/bin/activate
-
-echo "Start the listener"
-python -m civ_vi_webhook.services.matrix.matrix_bot_listener.py
--- start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Start the virtual environment."
-source venv/bin/activate
-
-# Defaults to port 8000
-# with --port integer, can change the port number
-echo "Start the server."
-uvicorn civ_vi_webhook.main:app --port 5000 --host 0.0.0.0 --reload
