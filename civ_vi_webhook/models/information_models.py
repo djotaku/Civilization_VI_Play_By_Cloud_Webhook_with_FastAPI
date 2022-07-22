9a31cd6f98b0d28bae1cad42617da553f244d01f
Eric Mesa <ericsbinaryworld@gmail.com>
2022-07-22 12:44:02 -0400

    Added example for the CurrentGames model

--- civ_vi_webhook/models/information_models.py
+++ civ_vi_webhook/models/information_models.py
@@ -26,3 +26,41 @@ class Game(BaseModel):
 class CurrentGames(BaseModel):
     """The response to a request for current games."""
     games: list[Game]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "games": [
+                    {
+                        "game_name": "Eric's Barbarian Clash Game",
+                        "game_info": {
+                            "player_name": "Eric",
+                            "turn_number": 300,
+                            "time_stamp": {
+                                "year": 2022,
+                                "month": 7,
+                                "day": 21,
+                                "hour": 20,
+                                "minute": 33,
+                                "second": 28
+                            }
+                        }
+                    },
+                    {
+                        "game_name": "Dan's Crazy Game",
+                        "game_info": {
+                            "player_name": "David",
+                            "turn_number": 5,
+                            "time_stamp": {
+                                "year": 2022,
+                                "month": 7,
+                                "day": 22,
+                                "hour": 12,
+                                "minute": 11,
+                                "second": 39
+                            }
+                        }
+                    }
+                ]
+            }
+        }
