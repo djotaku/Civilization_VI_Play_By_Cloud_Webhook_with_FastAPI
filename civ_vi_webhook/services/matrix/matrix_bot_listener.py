7e6e1e6c232ad06f5e1d90bb455bc8d593c34a28
Eric Mesa <ericsbinaryworld@gmail.com>
2022-07-26 19:14:23 -0400

    fixed completion response bug

--- civ_vi_webhook/services/matrix/matrix_bot_listener.py
+++ civ_vi_webhook/services/matrix/matrix_bot_listener.py
@@ -154,7 +154,7 @@ class ListenerMatrixBot:
         if response.status_code == 200:
             completion_status = response.json()
             logging.debug(f"{completion_status=}")
-            return f"Marked {completion_status['game_name']} as completed."
+            return f"Marked {completion_status['completed_game']['game_name']} as completed."
         elif response.status_code == 404:
             return f"{game_to_complete} was not found. Did you spell it correctly?"
 
