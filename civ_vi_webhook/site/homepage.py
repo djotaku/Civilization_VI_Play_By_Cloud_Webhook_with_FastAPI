9edaf5295042f04a7fc32566e41a6725dc1617bd
Eric Mesa <ericsbinaryworld@gmail.com>
2022-08-06 12:09:33 -0400

    Games are now sorted from oldest to newest
    Makes it easier to see who's holding things up.

--- civ_vi_webhook/site/homepage.py
+++ civ_vi_webhook/site/homepage.py
@@ -1,3 +1,5 @@
+import logging
+from collections import OrderedDict
 from fastapi import APIRouter
 from starlette.requests import Request
 
@@ -9,9 +11,10 @@ router = APIRouter(tags=['index'])
 def sort_games() -> (dict, dict):
     """Sort the games into current and completed."""
     all_games = load_most_recent_games()
+    sorted_by_timestamp = OrderedDict(sorted(all_games.items(), key=lambda k: int(f"{k[1]['time_stamp']['year']}{k[1]['time_stamp']['month']}{k[1]['time_stamp']['day']}{k[1]['time_stamp']['hour']}{k[1]['time_stamp']['minute']}{k[1]['time_stamp']['second']}")))
     current_games = {}
     completed_games = {}
-    for game in all_games:
+    for game in sorted_by_timestamp:
         if all_games[game].get("game_completed"):
             completed_games[game] = all_games[game]
         else:
