6d46eaa23c8637616ff83091eeb520f33706d3a9
Eric Mesa <ericsbinaryworld@gmail.com>
2022-08-06 12:15:38 -0400

    tried to fix the long line, but this is the best that can be done

--- civ_vi_webhook/site/homepage.py
+++ civ_vi_webhook/site/homepage.py
@@ -11,7 +11,8 @@ router = APIRouter(tags=['index'])
 def sort_games() -> (dict, dict):
     """Sort the games into current and completed."""
     all_games = load_most_recent_games()
-    sorted_by_timestamp = OrderedDict(sorted(all_games.items(), key=lambda k: int(f"{k[1]['time_stamp']['year']}{k[1]['time_stamp']['month']}{k[1]['time_stamp']['day']}{k[1]['time_stamp']['hour']}{k[1]['time_stamp']['minute']}{k[1]['time_stamp']['second']}")))
+    sorted_by_timestamp = OrderedDict(sorted(all_games.items(), key=lambda k: int(
+        f"{k[1]['time_stamp']['year']}{k[1]['time_stamp']['month']}{k[1]['time_stamp']['day']}{k[1]['time_stamp']['hour']}{k[1]['time_stamp']['minute']}{k[1]['time_stamp']['second']}")))
     current_games = {}
     completed_games = {}
     for game in sorted_by_timestamp:
