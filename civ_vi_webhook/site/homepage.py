426f500c31ac2370efc7aaa4975334e5788dd2e4
Eric Mesa <ericsbinaryworld@gmail.com>
2022-08-06 12:36:30 -0400

    I think I found the sorting issue
    It has to do with numbers <10

--- civ_vi_webhook/site/homepage.py
+++ civ_vi_webhook/site/homepage.py
@@ -8,11 +8,22 @@ from ..dependencies import templates, load_most_recent_games
 router = APIRouter(tags=['index'])
 
 
+def format_number(number: str) -> str:
+    """Take in a number than can potentially have a tens place and add a zero if needed"""
+    return f"{number:>2d}"
+
+
+def format_year_to_number(time_stamp: dict) -> int:
+    """Take in a dict with time stamp and convert to a number"""
+    return int(
+        f"{time_stamp['year']}{time_stamp['month']:>2d}{time_stamp['day']:>2d}{time_stamp['hour']:>2d}{time_stamp['minute']:>2d}{time_stamp['second']:>2d}")
+
+
 def sort_games() -> (dict, dict):
     """Sort the games into current and completed."""
     all_games = load_most_recent_games()
-    sorted_by_timestamp = OrderedDict(sorted(all_games.items(), key=lambda k: int(
-        f"{k[1]['time_stamp']['year']}{k[1]['time_stamp']['month']}{k[1]['time_stamp']['day']}{k[1]['time_stamp']['hour']}{k[1]['time_stamp']['minute']}{k[1]['time_stamp']['second']}")))
+    sorted_by_timestamp = OrderedDict(
+        sorted(all_games.items(), key=lambda k: format_year_to_number(k[1]['time_stamp'])))
     current_games = OrderedDict()
     completed_games = OrderedDict()
     for game in sorted_by_timestamp:
@@ -20,8 +31,8 @@ def sort_games() -> (dict, dict):
             completed_games[game] = sorted_by_timestamp[game]
         else:
             current_games[game] = sorted_by_timestamp[game]
-    logging.debug(f"{current_games=}")
-    logging.debug(completed_games)
+    # logging.debug(f"{current_games=}")
+    # logging.debug(completed_games)
     return completed_games, current_games
 
 
