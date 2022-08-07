27f8423e25a4e8a584d609f1df89bbebaca67da0
Eric Mesa <ericsbinaryworld@gmail.com>
2022-08-06 22:55:37 -0400

    getting closer

--- civ_vi_webhook/api/action_endpoints.py
+++ civ_vi_webhook/api/action_endpoints.py
@@ -42,3 +42,19 @@ def complete_game(game_to_complete: str = Query(None,
         json.dump(current_games, most_recent_games_file)
     completed_response = dict_to_game_model({f"{game_to_complete}": current_games[game_to_complete]})
     return {"completed_game": completed_response}
+
+
+@router.put('/set_winner', status_code=status.HTTP_200_OK, responses={status.HTTP_404_NOT_FOUND: {"model": Error}})
+def set_winner(game: str = Query(None, title="Game",
+                                 description="The name of the game to set the winner in"),
+               winner: str = Query(None, title="Winner",
+                                   description="The name of the winner of the game")):
+    current_games = load_most_recent_games()
+    if game not in current_games.keys():
+        return fastapi.responses.JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                                              content={"error": f"Game: {game} not found."})
+    current_games[game]['winner'] = winner
+    with open('most_recent_games.json', 'w') as most_recent_games_file:
+        json.dump(current_games, most_recent_games_file)
+    winner_response = dict_to_game_model({f"{game}": current_games[game]})
+    return {"winner_set": winner_response}
