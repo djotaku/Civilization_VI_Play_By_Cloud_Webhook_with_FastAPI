aa2460ba60c6ee31c17284933425db1d5dd762a3
Eric Mesa <ericsbinaryworld@gmail.com>
2022-08-20 15:26:41 -0400

    Turn Endpoints now all use the database

--- civ_vi_webhook/api/turn_endpoints.py
+++ civ_vi_webhook/api/turn_endpoints.py
@@ -12,8 +12,6 @@ from civ_vi_webhook.models.api.turns import CivTurnInfo, PYDTTurnInfo
 from civ_vi_webhook.services.matrix import matrix_bot_sender as matrix_bot
 from civ_vi_webhook.services.db import user_service, game_service
 
-from ..dependencies import load_most_recent_games
-
 router = APIRouter(tags=['Turn Endpoints'])
 
 # ##########
@@ -22,15 +20,15 @@ router = APIRouter(tags=['Turn Endpoints'])
 api_matrix_bot = matrix_bot.MatrixBot()
 
 
-def turn_delta(games: dict, this_game: str, current_time: datetime) -> float:
-    if this_game in games:
-        last_turn = datetime(int(games[this_game]['time_stamp']['year']),
-                             int(games[this_game]['time_stamp']['month']),
-                             int(games[this_game]['time_stamp']['day']),
-                             int(games[this_game]['time_stamp']['hour']),
-                             int(games[this_game]['time_stamp']['minute']),
-                             int(games[this_game]['time_stamp']['second']),
-                             )
+async def turn_delta(this_game: str, current_time: datetime) -> float:
+    if await game_service.check_for_game(this_game):
+        game = await game_service.get_game(this_game)
+        last_turn = datetime(game.game_info.time_stamp.year,
+                             game.game_info.time_stamp.month,
+                             game.game_info.time_stamp.day,
+                             game.game_info.time_stamp.hour,
+                             game.game_info.time_stamp.minute,
+                             game.game_info.time_stamp.second)
         return (current_time - last_turn).total_seconds()
     else:
         return 0
@@ -44,26 +42,9 @@ def get_average_time(turn_deltas: list) -> str:
     return f"{days} days, {hours} hours, {minutes} min, {seconds:.0f}s."
 
 
-@router.post('/webhook', status_code=status.HTTP_201_CREATED)
-async def handle_play_by_cloud_json(play_by_cloud_game: CivTurnInfo):
-    """The API endpoint for Civilization's Play By Cloud JSON data.
-
-    The reason for the duplication checks here are in case more than one player has the webhook enabled for all turns.
-    That may be desirable because, for example, right now Mac users have crashes when using the webhook.
-    """
-    send_message = False
-    current_games = load_most_recent_games()
-    turn_time = datetime.now()
-    api_logger.debug(f'JSON from Play By Cloud: {play_by_cloud_game}')
-    game_name = play_by_cloud_game.value1
-    time_since_last_turn = turn_delta(current_games, game_name, turn_time)
-    player = await user_service.get_user(play_by_cloud_game.value2)
-    if player.matrix_username:
-        player_name = player.matrix_username
-    else:
-        player_name = play_by_cloud_game.value2
-    api_logger.debug(f"{player_name=} if it's the steam username then either no matrix username or not in database")
-    turn_number = play_by_cloud_game.value3
+async def create_or_update_game(game_name: str, time_since_last_turn: float, player_id: str,
+                                turn_number: int, turn_time: datetime):
+    """Create or update a game in the database."""
     turn_deltas = []
     continuing_game: bool = await game_service.check_for_game(game_name)
     time_stamp = {'year': turn_time.year, 'month': turn_time.month, 'day': turn_time.day,
@@ -73,15 +54,34 @@ async def handle_play_by_cloud_json(play_by_cloud_game: CivTurnInfo):
         turn_deltas = game.game_info.turn_deltas
         turn_deltas.append(time_since_last_turn)
         average_turn_time = get_average_time(turn_deltas)
-        await game_service.update_game(game_name=game_name, player_id=str(player.id), turn_number=turn_number,
+        await game_service.update_game(game_name=game_name, player_id=player_id, turn_number=turn_number,
                                        time_stamp=time_stamp, turn_deltas=turn_deltas,
                                        average_turn_time=average_turn_time)
     else:
         turn_deltas.append(time_since_last_turn)
         average_turn_time = get_average_time(turn_deltas)
-        await game_service.create_game(game_name=game_name, player_id=str(player.id), turn_number=turn_number,
+        await game_service.create_game(game_name=game_name, player_id=player_id, turn_number=turn_number,
                                        time_stamp=time_stamp, turn_deltas=turn_deltas,
                                        average_turn_time=average_turn_time)
+
+
+@router.post('/webhook', status_code=status.HTTP_201_CREATED)
+async def handle_play_by_cloud_json(play_by_cloud_game: CivTurnInfo):
+    """The API endpoint for Civilization's Play By Cloud JSON data.
+
+    The reason for the duplication checks here are in case more than one player has the webhook enabled for all turns.
+    That may be desirable because, for example, right now Mac users have crashes when using the webhook.
+    """
+    turn_time = datetime.now()
+    api_logger.debug(f'JSON from Play By Cloud: {play_by_cloud_game}')
+    game_name = play_by_cloud_game.value1
+    time_since_last_turn = await turn_delta(game_name, turn_time)
+    player = await user_service.get_user(play_by_cloud_game.value2)
+    player_name = player.matrix_username or play_by_cloud_game.value2
+    api_logger.debug(f"{player_name=} if it's the steam username then either no matrix username or not in database")
+    turn_number = play_by_cloud_game.value3
+    player_id = str(player.id)
+    await create_or_update_game(game_name, time_since_last_turn, player_id, turn_number, turn_time)
 
     message = f"Hey, {player_name}, it's your turn in {game_name}. The game is on turn {turn_number}"
     await api_matrix_bot.send_message(message)
@@ -93,7 +93,9 @@ async def handle_play_by_cloud_json(play_by_cloud_game: CivTurnInfo):
 async def handle_pydt_json(pydt_game: PYDTTurnInfo):
     api_logger.debug(f'JSON from PYDT: {pydt_game}')
     game_name = pydt_game.gameName
-    player_name = await user_service.get_matrix_name(pydt_game.userName)
+    player = await user_service.get_user(pydt_game.userName)
+    player_name = player.matrix_username or pydt_game.userName
+    player_id = str(player.id)
     api_logger.debug(f"{player_name=} if it's the steam username then either no matrix username or not in database")
     turn_number = pydt_game.round
     civ_name = pydt_game.civName
@@ -102,28 +104,7 @@ async def handle_pydt_json(pydt_game: PYDTTurnInfo):
               f"The game is on turn {turn_number}"
     await api_matrix_bot.send_message(message)
     turn_time = datetime.now()
-    current_games = load_most_recent_games()
-    turn_deltas = []
-    time_since_last_turn = turn_delta(current_games, game_name, turn_time)
-    if game_name in current_games.keys():
-        if previous_deltas := current_games[game_name].get('turn_deltas'):
-            turn_deltas = list(previous_deltas)
-        turn_deltas.append(time_since_last_turn)
-    else:
-        turn_deltas.append(time_since_last_turn)
-    average_turn_time = get_average_time(turn_deltas)
-    current_games[game_name] = {'player_name': player_name,
-                                'turn_number': turn_number,
-                                'time_stamp': {'year': turn_time.year,
-                                               'month': turn_time.month,
-                                               'day': turn_time.day,
-                                               'hour': turn_time.hour,
-                                               'minute': turn_time.minute,
-                                               'second': turn_time.second},
-                                'turn_deltas': turn_deltas,
-                                'average_turn_time': average_turn_time
-                                }
-    with open('most_recent_games.json', 'w') as most_recent_games_file:
-        json.dump(current_games, most_recent_games_file)
+    time_since_last_turn = await turn_delta(game_name, turn_time)
+    await create_or_update_game(game_name, time_since_last_turn, player_id, turn_number, turn_time)
     return fastapi.responses.JSONResponse(status_code=status.HTTP_201_CREATED,
                                           content={"status": "Game Created"})
