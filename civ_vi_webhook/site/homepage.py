ee922dc501885f7d6e741f24bdaefd941032357c
Eric Mesa <ericsbinaryworld@gmail.com>
2022-08-26 14:02:55 -0400

    Implemented a potential fix.
    And added docstrings to all the homepage functions.

--- civ_vi_webhook/site/homepage.py
+++ civ_vi_webhook/site/homepage.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import fastapi.responses
 from fastapi import APIRouter
 from starlette.requests import Request
@@ -16,6 +18,7 @@ async def get_potential_winners_list() -> list:
 
 
 async def db_models_to_dictionary(games) -> list[dict]:
+    """Convert a game from a DB Model to a dictionary for use by Jinja2"""
     games_to_return = []
     for game in games:
         time_stamp = {"year": game.game_info.time_stamp.year,
@@ -35,18 +38,23 @@ async def db_models_to_dictionary(games) -> list[dict]:
 
 
 async def get_games_for_index() -> (list, list):
+    """Get the completed and current games into a list for the Jinja2 templates."""
     current_games_raw = await game_service.get_current_games()
     if current_games_raw:
         current_games = await db_models_to_dictionary(current_games_raw)
     else:
         current_games = []
     completed_games_raw = await game_service.get_completed_games()
-    completed_games = await db_models_to_dictionary(completed_games_raw)
+    if completed_games_raw:
+        completed_games = await db_models_to_dictionary(completed_games_raw)
+    else:
+        completed_games = []
     return completed_games, current_games
 
 
 @router.get('/')
 async def index(request: Request):
+    """Render the index page."""
     completed_games, current_games = await get_games_for_index()
     potential_winners = await get_potential_winners_list()
     return templates.TemplateResponse('index.html', {'request': request,
@@ -57,14 +65,23 @@ async def index(request: Request):
 
 @router.get('/favicon.ico')
 def favicon():
+    """Return a favicon.
+
+    Mostly just here to prevent 404s from not having one.
+    """
     return fastapi.responses.RedirectResponse(url="/static/favicon.ico")
 
 
 @router.put('/both_tables')
-async def get_both_games_tables(request: Request, game_to_complete: str):
+async def get_both_games_tables(request: Request, game_to_complete: Optional[str] = None):
+    """Gets the games for both tables and also allows marking a game as completed.
+
+    This is so that when a game is marked as completed, it re-renders both tables to make them seem interactive.
+    """
     completed_games, current_games = await get_games_for_index()
     potential_winners = await get_potential_winners_list()
-    await game_service.mark_game_completed(game_to_complete)
+    if game_to_complete is not None:
+        await game_service.mark_game_completed(game_to_complete)
     return templates.TemplateResponse('partials/both_tables.html', {'request': request,
                                                                     "current_games": current_games,
                                                                     "completed_games": completed_games,
@@ -73,6 +90,10 @@ async def get_both_games_tables(request: Request, game_to_complete: str):
 
 @router.get('/current_games_table')
 async def get_current_games_table(request: Request):
+    """Get the games for the current games table.
+
+    This allows us to use HTMX to render changes only to the current games.
+    """
     completed_games, current_games = await get_games_for_index()
     potential_winners = await get_potential_winners_list()
     return templates.TemplateResponse('partials/current_games_table.html', {'request': request,
@@ -83,6 +104,10 @@ async def get_current_games_table(request: Request):
 
 @router.get('/completed_games_table')
 async def get_completed_games_table(request: Request):
+    """Get the games for the completed games table.
+
+    This allows us to use HTMX to render changes only to the completed games table.
+    """
     completed_games, current_games = await get_games_for_index()
     potential_winners = await get_potential_winners_list()
     return templates.TemplateResponse('partials/completed_games_table.html', {'request': request,
